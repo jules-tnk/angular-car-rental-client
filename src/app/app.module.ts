fafc783301149d162b83995ed6c9424cdd6c981d
Kibalo Jules Tinaka <julestnk.dev@gmail.com>
2022-12-16 13:55:20 +0100

    main component and services created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { CarListComponent } from './car-list/car-list.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import { CarDetailComponent } from './car-detail/car-detail.component';
+import { ProfileComponent } from './profile/profile.component';
+import { HistoryComponent } from './history/history.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    CarListComponent,
+    SearchBarComponent,
+    CarDetailComponent,
+    ProfileComponent,
+    HistoryComponent
   ],
   imports: [
     BrowserModule,
